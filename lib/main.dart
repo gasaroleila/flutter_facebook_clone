cf4495c13af9acff2f93979ccff26153b5d4c7a4
gasaroleila <61538202+gasaroleila@users.noreply.github.com>
2022-02-28 22:05:04 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            // systemOverlayStyle: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text('facebook',
+            style: TextStyle(
+              color: Palette.facebookBlue,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
+            ),),
+          )
+        ],
+         
+      )
+    );
+  }
+}
