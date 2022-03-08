44eed33eef4909690ccce3c772887c4b2bc29cf9
gasaroleila <61538202+gasaroleila@users.noreply.github.com>
2022-03-09 00:04:59 +0200

    posts container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:facebook_clone/config/palette.dart';
 import 'package:facebook_clone/data/data.dart';
 import 'package:facebook_clone/widgets/create_post_contaier.dart';
+import 'package:facebook_clone/widgets/post_container.dart';
 import 'package:facebook_clone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:facebook_clone/models/models.dart';
@@ -65,17 +66,17 @@ class HomeScreen extends StatelessWidget {
               ),
               imageUrl:
                   'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-              isViewed: true,
+              isViewed: false,
             ),
             Story(
-              user: User(
-                name: 'Jessie Samson',
+                user: User(
+                  name: 'Jessie Samson',
+                  imageUrl:
+                      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                ),
                 imageUrl:
-                    'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-              ),
-              imageUrl:
-                  'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
-            ),
+                    'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+                isViewed: false),
             Story(
               user: User(
                 name: 'Jessie Samson',
@@ -84,19 +85,24 @@ class HomeScreen extends StatelessWidget {
               ),
               imageUrl:
                   'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-              isViewed: true,
+              isViewed: false,
             ),
             Story(
-              user: User(
-                name: 'Jessie Samson',
+                user: User(
+                  name: 'Jessie Samson',
+                  imageUrl:
+                      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+                ),
                 imageUrl:
-                    'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-              ),
-              imageUrl:
-                  'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
-            )
+                    'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+                isViewed: false)
           ])),
-        )
+        ),
+        // SliverList(
+        //     delegate: SliverChildBuilderDelegate((BuildContext context, index) {
+        //   final Post post = posts[index];
+        //   return PostContainer(post: post);
+        // }, childCount: posts.length))
       ],
     ));
   }
---
+++ lib/widgets/post_container.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:facebook_clone/models/models.dart';
+
+class PostContainer extends StatelessWidget {
+  final Post post;
+  const PostContainer({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
